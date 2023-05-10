0ffa463c56fc1895d3d8620d096054dcd9458f5b
tengyu <tengyu@tencent.com>
2023-05-10 16:17:14 +0800

    update

--- pages/api/knowledge-list.ts
+++ pages/api/knowledge-list.ts
@@ -1,19 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
+
 import fs from 'fs';
 
 const handler = async (req: NextApiRequest, res: NextApiResponse) => {
-    try {
-        // 判断是否存在knowledge.json文件，没有创建
-        const isExist = fs.existsSync('./knowledge.json');
-        if (!isExist) {
-            fs.writeFileSync('./knowledge.json', '[]');
-        }
-        // 读取knowledge.json文件
-        const configArrStr = fs.readFileSync('./knowledge.json', 'utf-8');
-        res.status(200).json({ success: true, data: JSON.parse(configArrStr) });
-    } catch (error) {
-        res.status(500).json({ success: false });
+  try {
+    // 判断是否存在knowledge.json文件，没有创建
+    const isExist = fs.existsSync('./knowledge.json');
+    if (!isExist) {
+      fs.writeFileSync('./knowledge.json', '[]');
     }
+    // 读取knowledge.json文件
+    const configArrStr = fs.readFileSync('./knowledge.json', 'utf-8');
+    res.status(200).json({ success: true, data: JSON.parse(configArrStr) });
+  } catch (error) {
+    res.status(500).json({ success: false, error });
+  }
 };
 
 export default handler;
